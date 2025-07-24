c4e900454d0433513a55f6abc35d2fde7b741e92
roshav00 <roshav00>
2025-07-24 11:09:51 +0000

    commit

---
+++ branch_1/Notebooks/01_Initialization/01_000_Schemas.py
@@ -0,0 +1,39 @@
+# Databricks notebook source
+from pyspark.sql.types import StructType, StructField, IntegerType, TimestampType, StringType
+
+error_log_schema = StructType([
+        StructField("error_id", StringType(), True),
+        StructField("job_run_id", StringType(), True),
+        StructField("error_timestamp", TimestampType(), True),
+        StructField("error_message", StringType(), True),
+        StructField("notebook_path", StringType(), True),
+        StructField("working_environment", StringType(), True),
+        StructField("error_type", StringType(), True),
+        StructField("error_description", StringType(), True),
+        StructField("error_category", StringType(), True),
+        StructField("user_id", StringType(), True)
+    ])
+
+# COMMAND ----------
+
+d=spark.createDataFrame([], error_log_schema)
+
+# COMMAND ----------
+
+d.show()
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, TimestampType, StringType
+
+event_log_schema = StructType([
+        StructField("job_run_id", StringType(), True),
+        StructField("event_timestamp", TimestampType(), True),
+        StructField("event_message", StringType(), True),
+        StructField("notebook_path", StringType(), True),
+        StructField("working_environment", StringType(), True),
+        StructField("event_type", StringType(), True),
+        StructField("event_description", StringType(), True),
+        StructField("event_category", StringType(), True),
+        StructField("user_id", StringType(), True)
+    ])
